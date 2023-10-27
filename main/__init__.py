d8055d35aeb74efb101d6a8670d26a67f29d75fc
githubmeking <130077291+githubmeking@users.noreply.github.com>
2023-10-27 18:47:23 +0300

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = 20213849
+API_HASH = "e97df0eca2a9531c80202c1a7d3f5721"
+BOT_TOKEN = "6666983690:AAE09spYtWpC-E68x2LL4wfafdrzexlEGNs"
+SESSION = "AQCnPNrkeL-ow9RAl21VKo3UtnLycHgIsQoiP5tQskt3O_hFLQWeBsHVLHA7WnmreLbzmck-NkS8M6pBj_w1O3AmPLfXofYFWp7o_eLY1vzKzxOAswmNThjkO9CFK13dqRPrcpkxZLONTbys3APP56yuYVzbrXS_UIUJjJjAvZ6XqUcdwteKtIpgtaH_td8TxLEs-jZtm8Mc8xOtrydHjIu_uXHy01jxMjr1yfJBs18fHvMG33KRB_QylXK14vQZhAksIyOPwK2gQdaussiIJ03nDPN84-XN_0bmmVHTFe3nBR_RsZobH1hm_mp-YMscdwZmqOcIagZ-jZdKNpmB30EDAAAAAWDlE2QA"
+FORCESUB = "Dizifilmarsivi"
+AUTH = 5920592740
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
